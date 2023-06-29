040140416a965f71841066b7d8e902b7634284cf
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-06-29 10:40:42 +0300

    Create contentOption.jsx

---
+++ src/contentOption.jsx
@@ -0,0 +1,36 @@
+const socials = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    instagram: "https://instagram.com",
+};
+
+const navElements = [
+  {
+    to: "",
+    name: "Home",
+  },
+  {
+    to: "about",
+    name: "About",
+  },
+  {
+    to: "skills",
+    name: "Skills",
+  },
+  {
+    to: "services",
+    name: "Services",
+  },
+  {
+    to: "portfolio",
+    name: "Portfolio",
+  },
+  {
+    to: "contact",
+    name: "Contact",
+  },
+];
+export {
+    socials, navElements
+};
