34cac88825c1e92156c55e11de9083101c2ae59a
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-06-29 22:00:56 +0300

    Update Footer.styled.jsx

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-import {
-  Container
-} from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 export const FooterWrapp = styled(Container)`
-    height: 100%;
-    text-align: center;
-    font-size: medium;
-    color: rgb(202, 200, 200);
-`
+ font-family: 'Sulphur Point', sans-serif;
+ height: 5vh;
+ text-align: center;
+ font-size: medium;
+ color: black;
+ ${'' /* color: rgb(202, 200, 200); */}
+`;
