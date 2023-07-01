5121fa41517b368ecb57d9310c62e2ac79a4d3fa
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-07-01 16:00:05 +0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,26 +1,49 @@
-import { navElements } from '../../contentOption';
-import {} from './Contact.styled';
+// import { navElements } from '../../contentOption';
+import {
+ Container,
+ TitleWrapp,
+ Title,
+ TitleLine,
+ Content,
+ TextWrapp,
+ TextWrappTitle,
+ TextContacts,
+ Text,
+} from './Contact.styled';
 
 const Contact = () => {
  return (
-  <>
-   <h1>Contact Me</h1>
-   <div>
-    <div>
-     <h2>Get in touch</h2>
-     <a href="/">Email: name@domain.com</a>
-     <a href="/">Phone: (555)123-4567</a>
-     <p>Hola Visitor !!</p>
-     <p>
-      Thank you for bumping in. If you have any queries/ideas/projects, feel
-      free to PING me. I am always open to discussion. You can contact me via
-      the Contact Form OR simply click on the social media icon if you are more
-      comfortable there.
-     </p>
-    </div>
+  <Container>
+   <TitleWrapp>
+    <Title>Contact Me</Title>
+    <TitleLine />
+   </TitleWrapp>
+
+   <Content>
+    <TextWrapp>
+     <TextWrappTitle>Get in touch</TextWrappTitle>
+     <TextContacts>
+      <a href="mailto:ostrynska.kateryna@gmail.com">
+       <strong>Email: </strong>ostrynska.kateryna@gmail.com
+      </a>
+
+      <a href="tel:+380973676912">
+       <strong>Phone: </strong>+38 097 367-69-12
+      </a>
+     </TextContacts>
+     <Text>
+      <p>Hola Visitor !</p>
+      <p>
+       Thank you for bumping in. If you have any queries/ideas/projects, feel
+       free to PING me. I am always open to discussion. You can contact me via
+       the Contact Form OR simply click on the social media icon if you are more
+       comfortable there.
+      </p>
+     </Text>
+    </TextWrapp>
     <div></div>
-   </div>
-  </>
+   </Content>
+  </Container>
  );
 };
 
