fa7f2b39c63ce49736a462d6ba05caebe968e35b
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-07-02 17:12:22 +0300

    add toast

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  useLocation
-} from "react-router-dom";
-import withRouter from "./hooks/withRouter";
-import AppRoutes from "./routes";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import withRouter from './hooks/withRouter';
+import AppRoutes from './routes';
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function _ScrollToTop(props) {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return props.children;
+ const { pathname } = useLocation();
+ useEffect(() => {
+  window.scrollTo(0, 0);
+ }, [pathname]);
+ return props.children;
 }
 const ScrollToTop = withRouter(_ScrollToTop);
 
 const App = () => {
-  return (
-    <Router>
-       <ScrollToTop>
-        <Header />
-        <AppRoutes />
-        <Footer />
-      </ScrollToTop>
-    </Router>
-  );
+ return (
+  <Router>
+   <ScrollToTop>
+    <Header />
+    <AppRoutes />
+    <Footer />
+   </ScrollToTop>
+   <ToastContainer />
+  </Router>
+ );
 };
 
 export default App;
