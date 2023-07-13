1807c2cc15fbe4c0b341d8b0cfbf63386f6d6577
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-07-13 11:10:42 +0300

    fix

--- src/contentOption.jsx
+++ src/contentOption.jsx
@@ -89,87 +89,15 @@ const customModalStyles = {
 const portfolioData = [
  {
   image:
-   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725593/Screenshot_2023-07-07_at_13.22.36_pnatnj.png',
-  title: 'Web-Studio',
-  link: 'https://ostrynska.github.io/project-webstudio/#en',
-  git: 'https://github.com/Ostrynska/project-webstudio',
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688496769/primechart_zd7kek.png',
+  title: 'Prime Chat',
+  link: 'https://prime-chat.netlify.app/',
+  git: 'https://github.com/Ostrynska/prime-chat',
   category: 'personal',
-  id: '0',
-  tools: 'HTML5, SASS, JS, Mobile-first, Adaptive design',
+  id: '8',
+  tools: 'React, ',
   description:
-   'First personal project. Web-studio is a website with a responsive layout. In addition, created a language switcher (en-ua), content filter by category.',
- },
- {
-  image:
-   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725592/Screenshot_2023-07-07_at_13.23.14_hcqumi.png',
-  title: 'Ice Cream Shop',
-  link: 'https://samusev-anton.github.io/ice-cream-n/',
-  git: 'https://github.com/Samusev-Anton/ice-cream-n',
-  category: 'team',
-  id: '1',
-  tools: 'HTML5, SASS, JS, Mobile-first, Adaptive design, Parsel',
-  description:
-   'Team project, role: developer. Adaptive website for the retail chain of stores. I managed to work on the Contacts section and element on the layout that hasn’t any stylization (modal window in the section About) and Scroll to Top button.',
- },
- {
-  image:
-   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725594/Screenshot_2023-07-07_at_13.23.55_rvqsnc.png',
-  title: 'Filmoteka',
-  link: 'https://samusev-anton.github.io/filmoteka',
-  git: 'https://github.com/Samusev-Anton/filmoteka',
-  category: 'team',
-  id: '2',
-  tools: 'HTML5, SASS, JS, Mobile-first, Adaptive design, Parsel, REST API',
-  description:
-   'Team project, role: developer. Application for choosing a movie. I managed to work on positioning and styling the movie cards, as well as page pagination for search results, filters and popular movies.',
- },
- {
-  image:
-   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725147/Screenshot_2023-07-07_at_13.18.28_xakfdf.png',
-  title: 'Pixelstock',
-  link: 'https://pixelstock.netlify.app/',
-  git: 'https://github.com/Ostrynska/pixelstock',
-  category: 'personal',
-  id: '3',
-  tools: 'React, Mobile-first, Adaptive design, REST API',
-  description:
-   'Adaptive website for searching images by keyword and popular images when it loads. For HTTP requests, used the public image search service Pixabay. A modal window with a dark overlay should open on clicking a gallery item and display an image large version. Hovering over an image, shows the author, the number of likes, and the tags by which users can search for similar images.',
- },
- {
-  image:
-   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688811303/Screenshot_2023-07-08_at_13.14.46_uqzqyp.png',
-  title: 'Movie Room',
-  link: 'https://hdmovie-room.netlify.app',
-  git: 'https://github.com/Ostrynska/movie-room',
-  category: 'personal',
-  id: '4',
-  tools: 'React, Mobile-first, Adaptive design, REST API',
-  description:
-   'Adaptive website for searching and storing movies. For HTTP requests, the public service themoviedb.org API was used. To create a collection, a list of the most popular movies for today is rendered on the main page. It is also possible to search for a movie by keyword and request full movie information for the movie page (including cast and reviews).',
- },
- {
-  image:
-   'https://res.cloudinary.com/dcjlswtup/image/upload/v1689235420/Screenshot_2023-07-13_at_10.54.12_dupadq.png',
-  title: 'Phonebook',
-  link: 'https://demo-phonebook.netlify.app',
-  git: 'https://github.com/Ostrynska/phonebook',
-  category: 'personal',
-  id: '5',
-  tools: 'React, Mobile-first, Adaptive design, REST API',
-  description:
-   'Phonebook app that allows registration, login, and updating of users contacts, as well as working with a private collection of contacts.',
- },
- {
-  image:
-   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725594/Screenshot_2023-07-07_at_13.25.17_jmmx8l.png',
-  title: 'Recipes - So Yummy',
-  link: 'https://4106677.github.io/so-yummy-front-end/',
-  git: 'https://github.com/4106677/so-yummy-front-end',
-  category: 'team',
-  id: '6',
-  tools: 'React, Mobile-first, Adaptive design, REST API',
-  description:
-   '"What to cook?" is not only a recipe app, it is, in fact, your cookbook. You can add your own recipes to save them for the future. The application has been developed taking into account the adaptive layout from mobile devices to desktop.',
+   'Demo real-time chat application project to learn Socket.io (Full Stack Chatting App). The application has been developed taking into account the adaptive layout from mobile devices to desktops and built using React, Node.js. Uses Socket.io for real-time communication and stores user details in encrypted format in Mongo DB Database.',
  },
  {
   image:
@@ -185,15 +113,87 @@ const portfolioData = [
  },
  {
   image:
-   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688496769/primechart_zd7kek.png',
-  title: 'Prime Chat',
-  link: 'https://prime-chat.netlify.app/',
-  git: 'https://github.com/Ostrynska/prime-chat',
-  category: 'personal',
-  id: '8',
-  tools: 'React, ',
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725594/Screenshot_2023-07-07_at_13.25.17_jmmx8l.png',
+  title: 'Recipes - So Yummy',
+  link: 'https://4106677.github.io/so-yummy-front-end/',
+  git: 'https://github.com/4106677/so-yummy-front-end',
+  category: 'team',
+  id: '6',
+  tools: 'React, Mobile-first, Adaptive design, REST API',
   description:
-   'Demo real-time chat application project to learn Socket.io (Full Stack Chatting App). The application has been developed taking into account the adaptive layout from mobile devices to desktops and built using React, Node.js. Uses Socket.io for real-time communication and stores user details in encrypted format in Mongo DB Database.',
+   '"What to cook?" is not only a recipe app, it is, in fact, your cookbook. You can add your own recipes to save them for the future. The application has been developed taking into account the adaptive layout from mobile devices to desktop.',
+ },
+ {
+  image:
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1689235420/Screenshot_2023-07-13_at_10.54.12_dupadq.png',
+  title: 'Phonebook',
+  link: 'https://demo-phonebook.netlify.app',
+  git: 'https://github.com/Ostrynska/phonebook',
+  category: 'personal',
+  id: '5',
+  tools: 'React, Mobile-first, Adaptive design, REST API',
+  description:
+   'Phonebook app that allows registration, login, and updating of users contacts, as well as working with a private collection of contacts.',
+ },
+ {
+  image:
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688811303/Screenshot_2023-07-08_at_13.14.46_uqzqyp.png',
+  title: 'Movie Room',
+  link: 'https://hdmovie-room.netlify.app',
+  git: 'https://github.com/Ostrynska/movie-room',
+  category: 'personal',
+  id: '4',
+  tools: 'React, Mobile-first, Adaptive design, REST API',
+  description:
+   'Adaptive website for searching and storing movies. For HTTP requests, the public service themoviedb.org API was used. To create a collection, a list of the most popular movies for today is rendered on the main page. It is also possible to search for a movie by keyword and request full movie information for the movie page (including cast and reviews).',
+ },
+ {
+  image:
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725147/Screenshot_2023-07-07_at_13.18.28_xakfdf.png',
+  title: 'Pixelstock',
+  link: 'https://pixelstock.netlify.app/',
+  git: 'https://github.com/Ostrynska/pixelstock',
+  category: 'personal',
+  id: '3',
+  tools: 'React, Mobile-first, Adaptive design, REST API',
+  description:
+   'Adaptive website for searching images by keyword and popular images when it loads. For HTTP requests, used the public image search service Pixabay. A modal window with a dark overlay should open on clicking a gallery item and display an image large version. Hovering over an image, shows the author, the number of likes, and the tags by which users can search for similar images.',
+ },
+ {
+  image:
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725594/Screenshot_2023-07-07_at_13.23.55_rvqsnc.png',
+  title: 'Filmoteka',
+  link: 'https://samusev-anton.github.io/filmoteka',
+  git: 'https://github.com/Samusev-Anton/filmoteka',
+  category: 'team',
+  id: '2',
+  tools: 'HTML5, SASS, JS, Mobile-first, Adaptive design, Parsel, REST API',
+  description:
+   'Team project, role: developer. Application for choosing a movie. I managed to work on positioning and styling the movie cards, as well as page pagination for search results, filters and popular movies.',
+ },
+ {
+  image:
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725592/Screenshot_2023-07-07_at_13.23.14_hcqumi.png',
+  title: 'Ice Cream Shop',
+  link: 'https://samusev-anton.github.io/ice-cream-n/',
+  git: 'https://github.com/Samusev-Anton/ice-cream-n',
+  category: 'team',
+  id: '1',
+  tools: 'HTML5, SASS, JS, Mobile-first, Adaptive design, Parsel',
+  description:
+   'Team project, role: developer. Adaptive website for the retail chain of stores. I managed to work on the Contacts section and element on the layout that hasn’t any stylization (modal window in the section About) and Scroll to Top button.',
+ },
+ {
+  image:
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1688725593/Screenshot_2023-07-07_at_13.22.36_pnatnj.png',
+  title: 'Web-Studio',
+  link: 'https://ostrynska.github.io/project-webstudio/#en',
+  git: 'https://github.com/Ostrynska/project-webstudio',
+  category: 'personal',
+  id: '0',
+  tools: 'HTML5, SASS, JS, Mobile-first, Adaptive design',
+  description:
+   'First personal project. Web-studio is a website with a responsive layout. In addition, created a language switcher (en-ua), content filter by category.',
  },
 ];
 
