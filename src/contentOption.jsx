6dd73c3a27336ffbb658e65d2dfc2d9d29cf810c
Ostrynska <kateriinag@gmail.com>
2024-10-07 13:46:35 +0300

    Update contentOption.jsx

--- src/contentOption.jsx
+++ src/contentOption.jsx
@@ -109,6 +109,17 @@ const portfolioData = [
  {
   image:
    'https://res.cloudinary.com/dcjlswtup/image/upload/v1715256975/Screenshot_2024-05-09_at_15.04.46_bekvzt.png',
+  title: 'Recipe App',
+  link: 'https://level-food.vercel.app/',
+  git: 'https://github.com/Ostrynska/level-food',
+  category: 'personal',
+  id: '12',
+  tools: [next],
+  description: `The app is a quiz application that tests users' knowledge of certain issues.. It's set to transform traditional learning and testing methods, making the process more enjoyable, efficient, and conducive to continuous.`,
+ },
+ {
+  image:
+   'https://res.cloudinary.com/dcjlswtup/image/upload/v1715256975/Screenshot_2024-05-09_at_15.04.46_bekvzt.png',
   title: 'Quiz App',
   link: 'https://next-quiz-app-nu.vercel.app/',
   git: 'https://github.com/Ostrynska/next-quiz-app',
